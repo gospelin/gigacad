5fa77edca99b8cf71d516d877f630804187a641c
gospelin <gospelin.gi@gmail.com>
2024-05-05 08:29:11 +0100

    Update routes.py to render index.html template
    Modified the routes defined in outes.py within the development branch to render the index.html template using Flask's
    render_template function. This ensures that the home page of the application is served with the content defined in the
    HTML template.

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
 from application import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
