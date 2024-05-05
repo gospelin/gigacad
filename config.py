5c07bead4f368a1426fa4be0f9fc723539ca239b
gospelin <gospelin.gi@gmail.com>
2024-05-05 17:14:47 +0100

    Update config.py with database configuration
    Updated the config.py file in the development branch to include database configuration settings.
    Added the SECRET_KEY for session management and configured the SQLALCHEMY_DATABASE_URI to connect to a
    MySQL database named 'school_database' on the specified hostname with the provided credentials.

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql://gigo:Tripled@121@hostname:3306/school_database"
+    )
