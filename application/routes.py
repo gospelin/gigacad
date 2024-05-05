1f5566d82b5962eee94cde723f7521b401bc633c
gospelin <gospelin.gi@gmail.com>
2024-05-05 17:09:11 +0100

    Update routes.py with additional route handlers
    Modified the routes in outes.py within the development branch to include additional route handlers.
    Added routes for '/home' and '/about_us', each rendering their respective HTML templates using Flask's
    ender_template function. This enhances the functionality of the application by providing more routes
    for different pages.

--- application/routes.py
+++ application/routes.py
@@ -4,5 +4,10 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
     return render_template('index.html')
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
