885f83a2b69cfd2845fefeb3035ff1bf0f1dcd1b
gospelin <gospelin.gi@gmail.com>
2024-05-05 17:07:04 +0100

    Update __init__.py with Flask and SQLAlchemy initialization
    Updated the __init__.py file in the development branch to initialize Flask and SQLAlchemy. Added the
    configuration from the Config class and initialized the Flask app and SQLAlchemy database object. Also
    imported the routes module to set up the application routes.

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+db = SQLAlchemy(app)
 from application import routes
 
 if __name__ == "__main__":
