07dcc390f460b0957f3f0a6a0010b2ad62254e8a
gospelin <gospelin.gi@gmail.com>
2024-05-09 05:51:20 +0100

    Update __init__.py with Flask app initialization and database migration setup
    Updated the __init__.py file in the pplication directory of the 'development' branch. Added Flask app
    initialization, configuration loading from Config class, SQLAlchemy database setup using db object, Flask-Migrate
    setup for database migrations, and import of routes. This configuration ensures proper initialization of the Flask app,
    database connection, and migration capabilities for managing database schema changes. Debug mode is enabled for
    development purposes. These changes enhance the application's structure and prepare it for database migrations
    and route handling.

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
@@ -7,6 +8,8 @@ app.config.from_object(Config)
 
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 from application import routes
 
 if __name__ == "__main__":
