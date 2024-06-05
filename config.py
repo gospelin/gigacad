226f1bd62993eca3bc28769c34cce5b8f797142f
gospelin <gospelin.gi@gmail.com>
2024-06-05 05:54:36 +0000

    changes

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser("C:/Users/GIGO/Documents/Flask_Development/AAIS")
-load_dotenv(os.path.join(project_folder, ".env"))
-
-
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") 
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     # or "your_secret_key_here"
 
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
