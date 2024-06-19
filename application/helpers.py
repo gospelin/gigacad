ae1eb587eb345f4f4b48c9ac7f60e228a6936ca0
gospelin <gospelin.gi@gmail.com>
2024-06-19 01:28:31 +0100

    Development

---
+++ application/helpers.py
@@ -0,0 +1,68 @@
+from .models import Student
+import random, string
+
+
+def generate_unique_username(first_name, last_name):
+    username = f"{first_name.strip().lower()}.{last_name.strip().lower()}"
+    existing_user = Student.query.filter_by(username=username).first()
+    if existing_user:
+        random_suffix = "".join(
+            random.choices(string.ascii_lowercase + string.digits, k=4)
+        )
+        username = f"{username}{random_suffix}"
+    return username
+
+
+def calculate_grade(total):
+    if total >= 95:
+        return "A+"
+    elif total >= 80:
+        return "A"
+    elif total >= 70:
+        return "B+"
+    elif total >= 65:
+        return "B"
+    elif total >= 60:
+        return "C+"
+    elif total >= 50:
+        return "C"
+    elif total >= 40:
+        return "D"
+    elif total >= 30:
+        return "E"
+    else:
+        return "F"
+
+
+def get_remark(total):
+    if total >= 95:
+        return "Outstanding"
+    elif total >= 80:
+        return "Excellent"
+    elif total >= 70:
+        return "Very Good"
+    elif total >= 65:
+        return "Good"
+    elif total >= 60:
+        return "Credit"
+    elif total >= 50:
+        return "Credit"
+    elif total >= 40:
+        return "Poor"
+    elif total >= 30:
+        return "Very Poor"
+    else:
+        return "Failed"
+
+
+# routes.py
+def calculate_grand_total(results):
+    return sum(result.total for result in results)
+
+
+def calculate_average(results):
+    total_sum = sum(result.total for result in results)
+    num_subjects = len(results)
+    if num_subjects == 0:
+        return 0
+    return total_sum / num_subjects
