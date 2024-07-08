d3bc8ba382ddb8db10d932d80b2a0119ff57882b
gospelin <gospelin.gi@gmail.com>
2024-07-08 03:35:00 +0100

    Modify helpers.py to execute more functions

--- application/helpers.py
+++ application/helpers.py
@@ -1,5 +1,32 @@
-from .models import Student
-import random, string
+import random, string, time
+from . import db
+from flask import request, abort
+from .models import Student, Subject, Result
+from sqlalchemy.exc import SQLAlchemyError
+from functools import wraps
+
+login_attempts = {}
+
+
+def rate_limit(limit, per):
+    def decorator(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            ip = request.remote_addr
+            now = time.time()
+            attempts = login_attempts.get(ip, [])
+            attempts = [timestamp for timestamp in attempts if now - timestamp < per]
+
+            if len(attempts) >= limit:
+                abort(429)  # Too Many Requests
+
+            attempts.append(now)
+            login_attempts[ip] = attempts
+            return f(*args, **kwargs)
+
+        return wrapped
+
+    return decorator
 
 
 def generate_unique_username(first_name, last_name):
@@ -55,7 +82,6 @@ def get_remark(total):
         return "Failed"
 
 
-# routes.py
 def calculate_grand_total(results):
     return sum(result.total for result in results)
 
@@ -75,20 +101,9 @@ def calculate_average(results):
 
 
 def calculate_cumulative_average(results, current_term_average):
-    """
-    Calculate the cumulative average based on the previous term's average and the current term's average.
-
-    Args:
-        results (list): A list of result objects, each having a 'last_term_average' attribute.
-        current_term_average (float): The average for the current term.
-
-    Returns:
-        float: The calculated cumulative average.
-    """
     last_term_average = 0
     cumulative_average = current_term_average
     if results:
-        # Assuming results is a list of objects with a 'last_term_average' attribute
         last_term_average = (
             float(results[0].last_term_average) if results[0].last_term_average else 0
         )
@@ -97,3 +112,82 @@ def calculate_cumulative_average(results, current_term_average):
         cumulative_average = (last_term_average + current_term_average) / 2
 
     return cumulative_average
+
+
+def get_subjects_by_entry_class(entry_class):
+    if "Nursery" in entry_class:
+        return Subject.query.filter_by(section="Nursery").all()
+    elif "Basic" in entry_class:
+        return Subject.query.filter_by(section="Basic").all()
+    else:
+        return Subject.query.filter_by(section="Secondary").all()
+
+
+def update_results(student, subjects, term, session, form):
+    try:
+        for subject in subjects:
+            class_assessment = request.form.get(f"class_assessment_{subject.id}", 0)
+            summative_test = request.form.get(f"summative_test_{subject.id}", 0)
+            exam = request.form.get(f"exam_{subject.id}", 0)
+            total = int(class_assessment) + int(summative_test) + int(exam)
+            grade = calculate_grade(total)
+            remark = get_remark(total)
+
+            result = Result.query.filter_by(
+                student_id=student.id, subject_id=subject.id, term=term, session=session
+            ).first()
+            if not result:
+                result = Result(
+                    student_id=student.id,
+                    subject_id=subject.id,
+                    term=term,
+                    session=session,
+                    class_assessment=class_assessment,
+                    summative_test=summative_test,
+                    exam=exam,
+                    total=total,
+                    grade=grade,
+                    remark=remark,
+                    next_term_begins=form.next_term_begins.data,
+                    last_term_average=form.last_term_average.data,
+                    position=form.position.data,
+                )
+                db.session.add(result)
+            else:
+                result.class_assessment = class_assessment
+                result.summative_test = summative_test
+                result.exam = exam
+                result.total = total
+                result.grade = grade
+                result.remark = remark
+                result.next_term_begins = form.next_term_begins.data
+                result.last_term_average = form.last_term_average.data
+                result.position = form.position.data
+        db.session.commit()
+    except SQLAlchemyError:
+        db.session.rollback()
+        raise
+
+
+def calculate_results(student_id, term, session):
+    results = Result.query.filter_by(
+        student_id=student_id, term=term, session=session
+    ).all()
+    grand_total = calculate_grand_total(results)
+    average = round(calculate_average(results), 1)
+
+    last_term = get_last_term(term)
+    last_term_results = Result.query.filter_by(
+        student_id=student_id, term=last_term, session=session
+    ).all()
+    last_term_average = round(
+        calculate_average(last_term_results) if last_term_results else 0, 1
+    )
+
+    for res in results:
+        res.last_term_average = last_term_average
+
+    cumulative_average = round(calculate_cumulative_average(results, average), 1)
+    results_dict = {result.subject_id: result for result in results}
+
+    return results, grand_total, average, cumulative_average, results_dict
