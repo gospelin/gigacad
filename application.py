d7cb738ee40ea10adb1fb8432f0619882a8b11ed
gospelin <gospelin.gi@gmail.com>
2024-07-11 02:22:11 +0100

    Format code using pretty formatter and black formatter

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from application import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-    #app.run(host='app')
-    #from waitress import serve
+    # app.run(host='app')
+    # from waitress import serve
 
-    #serve(app, host="0.0.0.0", port=5000)
+    # serve(app, host="0.0.0.0", port=5000)
